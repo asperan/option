b3bec424e93ef56560c9c410a1e932f69635ba65
Alex Speranza <alex.speranza@studio.unibo.it>
2021-10-13 14:07:43 +0200

    Add documentation to Errors

--- source/errors.d
+++ source/errors.d
@@ -1,11 +1,17 @@
 module option.errors;
 
+/**
+ * Error thrown when a null value is input to Option.some.
+ */
 class SomeOfNullError : Error {
   this() {
     super("Option.some does not accept a 'null' value. If you do not know whether the value is 'null' or not, use 'Option.nullable' instead.");
   }
 }
 
+/**
+ * Error thrown when the method 'get' is called on an empty Option.
+ */
 class GetOnNoneError : Error {
   this() {
     super("Called method 'get' on empty Option object. Check the object emptyness, or use the method 'or' to provide a default value when the object is empty.");
