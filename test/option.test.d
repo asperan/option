2e7c9eb0b8d44ed088a2e1544a64ddf1267c82f1
Alex Speranza <alex.speranza@studio.unibo.it>
2021-10-13 13:23:19 +0200

    Move test file into new 'test' directory

--- test/option.test.d
+++ test/option.test.d
@@ -1,42 +1,42 @@
 module option_test;
 import option;
 
-class A {
+private class A {
   int c;
   this() {c = 5;}
 }
 
 unittest {
-  Option!int i = Option!int.Some(3);
+  Option!int i = Option!int.some(3);
   assert(!i.isEmpty);
   assert(i.get == 3);
 }
 
 unittest {
-  Option!int i = Option!int.None();
+  Option!int i = Option!int.none();
   assert(i.isEmpty);
 }
 
 unittest {
   import std.exception : assertThrown;
-  assertThrown!Error(Option!A.Some(null));
+  assertThrown!Error(Option!A.some(null));
 }
 
 unittest {
-  Option!A i = Option!A.Nullable(null);
+  Option!A i = Option!A.nullable(null);
   assert(i.isEmpty);
-  Option!A j = Option!A.Nullable(new A());
+  Option!A j = Option!A.nullable(new A());
   assert(!j.isEmpty);
 }
 
 unittest {
-  Option!bool i = Option!bool.Nullable(false);
+  Option!bool i = Option!bool.nullable(false);
   assert(!i.isEmpty);
 }
 
 unittest {
-  Option!int i = Option!int.Some(5);
+  Option!int i = Option!int.some(5);
   assert(i.or(0) == 5);
-  Option!int j = Option!int.None();
+  Option!int j = Option!int.none();
   assert(j.or(0) == 0);
 }
